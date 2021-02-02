3f5fa534ead1bdf5a046c88cdeba0c1483586f53
agohdasol <agohdasol@gmail.com>
2021-02-02 13:44:44 +0900

    SQLite 테스트

--- Tester/Program.cs
+++ Tester/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Data.SQLite;
 
 namespace Tester
 {
@@ -11,14 +12,34 @@ namespace Tester
   {
     static void Main(string[] args)
     {
-      //string lines = File.ReadAllText(@"C:\EmailWriter\WinFormEmailWriter\bin\Debug\netcoreapp3.1\검토의견.htm", Encoding.Default);
-      Stream htmlStream = new FileStream(@"C:\EmailWriter\WinFormEmailWriter\bin\Debug\netcoreapp3.1\검토의견.htm", FileMode.Open, FileAccess.Read);
-      StreamReader docStreamReader = new StreamReader(htmlStream, System.Text.Encoding.Default);
-      htmlStream.Position = 0;
+      SQLiteConnection.CreateFile("test.sqlite");
 
-      string strHtml = docStreamReader.ReadToEnd();
+      var conn = new SQLiteConnection("Data Source=test.sqlite;");
+      conn.Open();
 
-      Console.WriteLine(strHtml);
+      string sql = "create table members (name varchar(20), age int)";
+
+      var cmd = new SQLiteCommand(sql, conn);
+      int result = cmd.ExecuteNonQuery();
+
+      sql = "create index idx01 on members(name)";
+      cmd = new SQLiteCommand(sql, conn);
+      result = cmd.ExecuteNonQuery();
+
+      sql = "insert into members (name, age) values ('황다솔', 30)";
+      cmd = new SQLiteCommand(sql, conn);
+      result = cmd.ExecuteNonQuery();
+
+      sql = "select * from members";
+      cmd = new SQLiteCommand(sql, conn);
+      SQLiteDataReader rdr = cmd.ExecuteReader();
+      while (rdr.Read())
+      {
+        Console.WriteLine(rdr["name"] + " " + rdr["age"]);
+      }
+      rdr.Close();
+
+      conn.Close();
     }
   }
 }
